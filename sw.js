02e2391ef31eb2910e319625969819502eb7d0a1
Ramenlover123 <laetitia@castellano.de>
2026-07-18 01:28:33 +0900

    v21

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    (v1 -> v2 ...). Dann holt sich das Handy beim nächsten Öffnen mit
    Internet die neue Version. */
 
-const CACHE = 'ima-v20';
+const CACHE = 'ima-v21';
 
 const ASSETS = [
   './',
